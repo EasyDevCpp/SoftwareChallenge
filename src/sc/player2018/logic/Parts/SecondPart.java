67b08cf5e9e49a8fac290a5c3af0ae7f1a4ed308
Unknown <manuel-serret@t-online.de>
2017-11-28 12:55:05 +0100

    Fixing #17

--- src/sc/player2018/logic/Parts/SecondPart.java
+++ src/sc/player2018/logic/Parts/SecondPart.java
@@ -20,33 +20,25 @@ public class SecondPart extends Part{
         enemy_fields[3]=enemyOnNextFieldType(FieldType.POSITION_2);
         enemy_fields[4]=enemyOnNextFieldType(FieldType.SALAD);
         enemy_fields[5]=enemyOnNextFieldType(FieldType.CARROT);
-
-        if(super.getPlayer().getCarrots()>10) {
-            if(!enemy_fields[4]&&super.getKarrotCosts()[distances[4]]<=super.getPlayer().getCarrots()) {
-                super.getActions().add(new Advance(distances[4]));
-                super.setNewTask(1);
-            } else if((!enemy_fields[2]&&super.getKarrotCosts()[distances[2]]<=super.getPlayer().getCarrots())&&(!enemy_fields[3]&&super.getKarrotCosts()[distances[3]]<=super.getPlayer().getCarrots())) {
-                if(super.getPlayer().getFieldIndex()>super.getEnemy().getFieldIndex()) super.getActions().add(new Advance(distances[2]));
-                else if(super.getPlayer().getFieldIndex()<super.getEnemy().getFieldIndex()) super.getActions().add(new Advance(distances[3]));
-            } else if(!enemy_fields[5]&&super.getKarrotCosts()[distances[5]]<=super.getPlayer().getCarrots()&&!(super.getPlayer().getLastNonSkipAction() instanceof ExchangeCarrots)) {
-                super.getActions().add(new Advance(distances[5]));
-                if(super.getPlayer().getCarrots()<90) super.setNewTask(2);
-                else super.setNewTask(3);
-            } else if(!enemy_fields[1]&&super.getKarrotCosts()[distances[1]]<=super.getPlayer().getCarrots()&&(super.getPlayer().ownsCardOfType(CardType.EAT_SALAD)||super.getPlayer().ownsCardOfType(CardType.TAKE_OR_DROP_CARROTS)||super.getPlayer().ownsCardOfType(CardType.HURRY_AHEAD))) {
-                super.getActions().add(new Advance(distances[1], 0));
-                if(super.getPlayer().ownsCardOfType(CardType.EAT_SALAD)) super.getActions().add(new Card(CardType.EAT_SALAD,1));
-                else if(super.getPlayer().getCarrots()<50&&super.getPlayer().ownsCardOfType(CardType.TAKE_OR_DROP_CARROTS)) super.getActions().add(new Card(CardType.TAKE_OR_DROP_CARROTS, 20,1));
-                else if(super.getPlayer().getCarrots()>50&&super.getPlayer().ownsCardOfType(CardType.TAKE_OR_DROP_CARROTS)) super.getActions().add(new Card(CardType.TAKE_OR_DROP_CARROTS, -20,1));
-                else if(enemy_fields[0]&&super.getPlayer().ownsCardOfType(CardType.HURRY_AHEAD)) super.getActions().add(new Card(CardType.HURRY_AHEAD,1));
-            } else {
-                super.getActions().add(new Skip(1));
-            }
+        
+        if(!enemy_fields[4]&&super.getKarrotCosts()[distances[4]]<=super.getPlayer().getCarrots()) {
+            super.getActions().add(new Advance(distances[4]));
+            super.setNewTask(1);
+        } else if((!enemy_fields[2]&&super.getKarrotCosts()[distances[2]]<=super.getPlayer().getCarrots())&&(!enemy_fields[3]&&super.getKarrotCosts()[distances[3]]<=super.getPlayer().getCarrots())) {
+            if(super.getPlayer().getFieldIndex()>super.getEnemy().getFieldIndex()) super.getActions().add(new Advance(distances[2]));
+            else if(super.getPlayer().getFieldIndex()<super.getEnemy().getFieldIndex()) super.getActions().add(new Advance(distances[3]));
+        } else if(!enemy_fields[5]&&super.getKarrotCosts()[distances[5]]<=super.getPlayer().getCarrots()&&!(super.getPlayer().getLastNonSkipAction() instanceof ExchangeCarrots)) {
+            super.getActions().add(new Advance(distances[5]));
+            if(super.getPlayer().getCarrots()<90) super.setNewTask(2);
+            else super.setNewTask(3);
+        } else if(!enemy_fields[1]&&super.getKarrotCosts()[distances[1]]<=super.getPlayer().getCarrots()&&(super.getPlayer().ownsCardOfType(CardType.EAT_SALAD)||super.getPlayer().ownsCardOfType(CardType.TAKE_OR_DROP_CARROTS)||super.getPlayer().ownsCardOfType(CardType.HURRY_AHEAD))) {
+            super.getActions().add(new Advance(distances[1], 0));
+            if(super.getPlayer().ownsCardOfType(CardType.EAT_SALAD)) super.getActions().add(new Card(CardType.EAT_SALAD,1));
+            else if(super.getPlayer().getCarrots()<50&&super.getPlayer().ownsCardOfType(CardType.TAKE_OR_DROP_CARROTS)) super.getActions().add(new Card(CardType.TAKE_OR_DROP_CARROTS, 20,1));
+            else if(super.getPlayer().getCarrots()>50&&super.getPlayer().ownsCardOfType(CardType.TAKE_OR_DROP_CARROTS)) super.getActions().add(new Card(CardType.TAKE_OR_DROP_CARROTS, -20,1));
+            else if(enemy_fields[0]&&super.getPlayer().ownsCardOfType(CardType.HURRY_AHEAD)) super.getActions().add(new Card(CardType.HURRY_AHEAD,1));
         } else {
-            if(b.getPreviousFieldByType(FieldType.CARROT,super.getPlayer().getFieldIndex())!=super.getEnemy().getFieldIndex()) {
-                super.getActions().add(new FallBack(0));
-            } else {
-                super.getActions().add(new Skip(1));
-            }
+            super.getActions().add(new Skip(1));
         }
     }
 }
