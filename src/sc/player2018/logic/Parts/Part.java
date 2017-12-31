e8b2b073349b964afb0b3388854250f3408379eb
Unknown <manuel-serret@t-online.de>
2017-12-31 11:18:16 +0100

    Fixed: #15, #27

--- src/sc/player2018/logic/Parts/Part.java
+++ src/sc/player2018/logic/Parts/Part.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 public abstract class Part {
     private Move m;
     private ArrayList<Action> actions;
-    private int newTask;
+    private static int newTask;
     private int[] karrotCosts = {0,1,3,6,10,15,21,28,36,45,55,66,78,91,105,120,136,153,171,190,210,231,253,276,300,325,351,378,406,435,465,496,528,561,595,630,666,703,741,780,820,861,903,946,990};
     private GameState gs;
     private Player player;
